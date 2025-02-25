58b441bb034bef4193c6ce6bc770b0d7af60d21f
SHIVANSH-A <shivansh10dubey@gmail.com>
2025-02-25 20:14:49 +0530

    Corrected Code

--- Problems/UserActivityforthePast30DaysI.sql
+++ Problems/UserActivityforthePast30DaysI.sql
@@ -1,12 +1,12 @@
-// This table may have duplicate rows.
-// The activity_type column is an ENUM (category) of type ('open_session', 'end_session', 'scroll_down', 'send_message').
-// The table shows the user activities for a social media website. 
-// Note that each session belongs to exactly one user.
+-- // This table may have duplicate rows.
+-- // The activity_type column is an ENUM (category) of type ('open_session', 'end_session', 'scroll_down', 'send_message').
+-- // The table shows the user activities for a social media website. 
+-- // Note that each session belongs to exactly one user.
  
 
-// Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
+-- // Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
 
-// Return the result table in any order.
+-- // Return the result table in any order.
 /* Write your PL/SQL query statement below */
 SELECT TO_CHAR(activity_date,'YYYY-MM-DD') as day,count(distinct(user_id)) active_users from Activity
 WHERE activity_type = 'open_session' or activity_type = 'scroll_down' or
