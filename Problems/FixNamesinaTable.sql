750956b11b358f2e8c2482f7ba5b7f654fcd6e72
SHIVANSH-A <shivansh10dubey@gmail.com>
2025-02-25 20:16:08 +0530

    1667 Leetcode

---
+++ Problems/FixNamesinaTable.sql
@@ -0,0 +1,10 @@
+-- user_id is the primary key (column with unique values) for this table.
+-- This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+ 
+
+-- Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+-- Return the result table ordered by user_id.
+--|| = concatenate
+
+select user_id, (upper(substr(name,1,1)) || lower(substr(name,2))) as name from users order by user_id;
